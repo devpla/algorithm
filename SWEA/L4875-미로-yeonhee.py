b8fdadeb6deced41e5764319c299e1ceb57b5615
hing9u <yeonheechae425@gmail.com>
2021-08-24 16:29:49 +0900

    refactor: Solve swea 4875 using recursion

--- SWEA/L4875-미로-yeonhee.py
+++ SWEA/L4875-미로-yeonhee.py
@@ -1,31 +1,38 @@
 import sys
 sys.stdin = open('input.txt')
 
-for t in range(1, int(input())+1):
-    n = int(input())
-    maze = [list(map(int, input())) for _ in range(n)]
-    directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-    visited = [[0] * n for _ in range(n)]
-    result = 0
+MOVES = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-    # 시작 인덱스 찾기
+
+def find_start():
     for r in range(n):
         for c in range(n):
-            if maze[r][c] == 2:
-                visited[r][c] = 1
-                stack = [(r, c)]
+            if maze[r][c] == '2':
+                return r, c
 
-    # 미로 찾기
-    while stack:
-        r, c = stack.pop()
-        if maze[r][c] == 3:
-            result = 1
-            break
-        for dr, dc in directions:
-            nr = r + dr
-            nc = c + dc
-            if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc] and maze[nr][nc] in (0, 3):
-                stack.append((nr, nc))
+
+def dfs(r, c):
+    global result
+    for dr, dc in MOVES:
+        nr = r + dr
+        nc = c + dc
+        if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc]:
+            # 도착
+            if maze[nr][nc] == '3':
+                result = 1
+            elif maze[nr][nc] == '0':
                 visited[nr][nc] = 1
+                dfs(nr, nc)
 
-    print(f'#{t} {result}')
+
+for t in range(1, int(input())+1):
+
+    n = int(input())
+    maze = [input().rstrip() for _ in range(n)]
+    visited = [[0]*n for _ in range(n)]
+
+    r, c = find_start()
+    result = 0
+    dfs(r, c)
+
+    print(f'#{t}', result)
