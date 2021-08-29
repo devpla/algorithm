f262b6edce43317345e78852f4d3b05c61c5add2
hing9u <yeonheechae425@gmail.com>
2021-08-29 22:36:01 +0900

    code: Solve boj 2669 직사각형 네개의 합집합의 면적 구하기

---
+++ BOJ/02669-직사각형_네개의_합집합의_면적_구하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+square = [[0] * 101 for _ in range(101)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+
+    for y in range(y1, y2):
+        for x in range(x1, x2):
+            square[y][x] = 1
+
+print(sum(map(sum, square)))
